6c1d44580852863a0937bbfdd27dfb512d83475e
midnight-plat <midnight@platcore.io>
2025-11-11 20:12:38 +0300

    add folder lecture_1

--- lecture_1/main.py
+++ lecture_1/main.py
@@ -4,12 +4,11 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 from colorama import init, Fore, Style, Back  # import from library
+
 init()
 print(f"{Fore.RED}{Back.YELLOW}Hello World!{Style.RESET_ALL}")
 print(f"{Fore.GREEN}Hello World in Green!{Style.RESET_ALL}")
 print(f"{Fore.BLUE}{Style.BRIGHT}Hello World in Bright Blue!{Style.RESET_ALL}")
 print(f"{Fore.MAGENTA}{Back.CYAN}Hello World with Magenta text and Cyan background!{Style.RESET_ALL}")
 
-
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
