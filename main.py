24e2716e4893b62214b426220836454c56493973
midnight-plat <midnight@platcore.io>
2025-11-11 19:58:11 +0300

    Colored text in main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+from colorama import init, Fore, Style, Back  # import from library
+init()
+print(f"{Fore.RED}{Back.YELLOW}Hello World!{Style.RESET_ALL}")
+print(f"{Fore.GREEN}Hello World in Green!{Style.RESET_ALL}")
+print(f"{Fore.BLUE}{Style.BRIGHT}Hello World in Bright Blue!{Style.RESET_ALL}")
+print(f"{Fore.MAGENTA}{Back.CYAN}Hello World with Magenta text and Cyan background!{Style.RESET_ALL}")
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
